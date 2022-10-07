f0b36b705d9f66a43cc6c5b3ba80b95f35c03e4d
Dani Salanova <dasadmx@gmail.com>
2022-10-07 22:32:16 +0200

    Base + Ejercicio 2 y 5

---
+++ my_lib.c
@@ -0,0 +1,61 @@
+#include "my_lib.h"
+
+size_t my_strlen(const char *str) {
+   size_t len = 0;
+   int i = 0;
+ 
+   while (str[i]) { // o while (str[i]!='\0')
+           i++;
+           len++;
+   }
+   return len;
+}
+
+
+int my_strcmp(const char *str1, const char *str2){
+    int cont = 0;
+    int i = 0;
+    for(; str1[i] && str2[i]; i++){
+        if(str1[i] - str2[i] == 0){
+            cont++;
+        }else if(str1[i] - str2[i] > 0){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    //Si salimos puede ser porque en uno no hay datos o porque son iguales
+    if(str1[i] && !str2[i]){
+        return 1;
+    }else if (!str1[i] && str2[i]){
+        return -1;
+    }else if (cont == my_strlen(str1)){
+        return 0;
+    }
+}
+
+char *my_strcat(char *dest, const char *src){
+    int posNull = 0;
+    while(dest[posNull]){ //Hayamos el caracter \0
+        posNull++;
+    }
+    printf("Posicion del Null: %i", posNull);
+    //Ahora sabemos donde esta la posNull, procedemos ha añadir caracteres
+    int i = 0;
+    for(; src[i];i++ ){
+        dest[posNull++] = src[i];
+    }
+    return dest;
+}
+
+int main(){
+    //char cad1[5] = "apa";
+    //char cad2[5] = "apo";
+    //int a = my_strcmp(cad1,cad2);
+    //printf("Valor de la comparacion: %i", a);
+    char cad1[50] = "Inicio-";
+    char cad2[50] = "Final";
+    my_strcat(cad1,cad2);
+    printf("My frase queda como: %s", cad1);
+    return 0;
+}
